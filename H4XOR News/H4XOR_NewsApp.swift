f6ae17d23e2d61534936387c7606c8a840e93bfd
joomin <jmnl225@github.com>
2024-02-18 23:01:33 +0900

    Initial Commit

---
+++ H4XOR News/H4XOR_NewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  H4XOR_NewsApp.swift
+//  H4XOR News
+//
+//  Created by joomin on 2024/02/18.
+//
+
+import SwiftUI
+
+@main
+struct H4XOR_NewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
